f7c2192cbd55a67b1d1ee27d2c8c0f57d23d182c
treinamentos <treinamentos@UNI-LAB2-09>
2025-06-09 21:39:47 -0300

    Adicionar arquivos de projeto.

---
+++ SistemaAlunosPOO/Alunos.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaAlunosPOO
+{
+    internal class Alunos
+    {
+        public string Nome {  get; set; }
+        public string RA {  get; set; }
+        public int Idade { get; set; }
+    }
+
+
+}
---
+++ SistemaAlunosPOO/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace SistemaAlunosPOO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Alunos> alunos = new List<Alunos>();
+        bool executar = true;
+
+            while (executar) 
+            {
+                Console.WriteLine(" MENU DE OPÇÕES");
+                Console.WriteLine("1. Cadastrar Aluno");
+                Console.WriteLine("2. Listar Alunos");
+                Console.WriteLine("3. Alterar Aluno");
+                Console.WriteLine("4. Remover Aluno");
+                Console.WriteLine("5. Sair");
+                Console.Write("Escolha uma opção: ");
+                string opcao = Console.ReadLine();
+
+
+                switch (opcao)
+                {
+                    case "1":
+
+                        Alunos novoAluno = new Alunos();
+
+                        Console.WriteLine("Digite o RA do aluno:");
+                        novoAluno.RA = Console.ReadLine();
+
+                    case "2":
+
+                        Console.WriteLine("---- Lista de Alunos");                }
+
+        }
+
+    }
+}
